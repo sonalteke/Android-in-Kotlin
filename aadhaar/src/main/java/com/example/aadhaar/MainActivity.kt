fd71491b80e6a35fbcd9c2133bc864a11d1881c4
sonalteke <sonal.teke11@gmail.com>
2017-11-23 17:37:52 +0530

    Aadhaar with anko,picasso

--- aadhaar/src/main/java/com/example/aadhaar/MainActivity.kt
+++ aadhaar/src/main/java/com/example/aadhaar/MainActivity.kt
@@ -1,74 +1,171 @@
 package com.example.aadhaar
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.drawable.GradientDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
+import android.text.InputType
+import android.text.InputType.TYPE_CLASS_TEXT
+import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
 import android.view.Gravity
+import android.view.View
+import android.view.ViewManager
+import android.widget.Button
 import android.widget.Toast
+import com.example.aadhaar.R.id.*
 import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.ankoView
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
+
+
+    inline fun ViewManager.textInputEditText() = textInputEditText {}
+    inline fun ViewManager.textInputEditText(theme: Int = 0, init: TextInputEditText.() -> Unit) = ankoView({ TextInputEditText(it) }, theme, init)
+
+    inline fun ViewManager.textInputLayout() = textInputLayout {}
+    inline fun ViewManager.textInputLayout(theme: Int = 0, init: TextInputLayout.() -> Unit) = ankoView({ TextInputLayout(it) }, theme, init)
+
+    val ADHAR:Int=12
+    lateinit var adhar:TextInputLayout
+    lateinit var adharNum:TextInputEditText
+    lateinit var send: Button
+    lateinit var resend:Button
+    lateinit var otp:TextInputLayout
+    lateinit var otpNum:TextInputEditText
+    lateinit var verify:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         verticalLayout {
-            padding = dip(70)
-            editText {
-                id = R.id.edtAadhar
-                hint = "Enter Aadhar number:"
-                textSize = 20f
+            backgroundColor = Color.WHITE
+            lparams(width= matchParent,height = matchParent)
+            padding=dip(70)
+            gravity=Gravity.CENTER
+
+            adhar =textInputLayout{
+                adharNum = textInputEditText {
+                    id = R.id.txtNum
+                    textSize=20f
+                    hint="Enter Aadhar Number"
+                }
             }
 
             linearLayout {
-                gravity = Gravity.CENTER
-                padding = dip(20)
 
-                lparams(width = wrapContent, height = wrapContent)
-                button {
+                padding=dip(20)
+                gravity=Gravity.CENTER
+
+
+                send = button("Send OTP") {
                     id = R.id.btnSend
-                    text = "Send OTP"
-                    onClick {
-                        Toast.makeText(this@MainActivity,
-                                "send OTP",Toast.LENGTH_SHORT).show()
-                    }
+                    backgroundColor=Color.rgb(77, 182, 172)
+                    textColor = Color.rgb(0, 77, 64)
+                    typeface= Typeface.DEFAULT_BOLD
+
+//                     backgroundColor = Color.rgb(128, 203, 196)
+
+                }.lparams(width= wrapContent,height = wrapContent){
+                    horizontalMargin = dip(5)
                 }
-                button {
+
+
+                resend = button("Resend OTP") {
                     id = R.id.btnResend
-                    text = "Resend OTP"
+                    backgroundColor = Color.rgb(77, 182, 172)
+//                    backgroundColor = Color.rgb(128, 203, 196)
+                    textColor = Color.rgb(0, 77, 64)
+                    typeface= Typeface.DEFAULT_BOLD
+                    visibility=View.INVISIBLE
                     onClick {
-                        Toast.makeText(this@MainActivity,
-                                "Resend OTP",Toast.LENGTH_SHORT).show()
+                        background = buttonBg()
+                        toast("OTP Resend")
                     }
+                }.lparams(width= wrapContent,height = wrapContent){
+                    gravity = Gravity.CENTER
                 }
-            }
+
+        }
+
+
             verticalLayout {
                 gravity = Gravity.CENTER
                 padding = dip(30)
-                editText {
-                    id = R.id.edtOtp
-                    hint = "Enter OTP:"
-                    textSize = 20f
 
-                }.lparams(width = wrapContent,height = wrapContent)
+                otp =  textInputLayout {
+                    otpNum=  textInputEditText {
+                        id = R.id.txtOtp
+                        textSize=20f
+                        hint="Enter OTP"
+                        inputType = TYPE_CLASS_TEXT or TYPE_TEXT_VARIATION_PASSWORD
+                    }
+                    visibility=View.INVISIBLE
+                }
 
                 linearLayout {
-                    padding = dip(20)
 
+                    padding = dip(20)
                     lparams(width = wrapContent, height = wrapContent)
-                    button {
+
+                    verify=button("verify") {
                         id = R.id.btnVerify
                         text = "Verify"
+                        background=buttonBg()
+//                        backgroundColor = Color.rgb(77, 182, 172)
+                        textColor = Color.rgb(0, 77, 64)
+                        typeface= Typeface.DEFAULT_BOLD
+                        visibility=View.INVISIBLE
                         onClick {
-                            startActivity(Intent(this@MainActivity,
-                                    Verify2Activity::class.java))
+                            if(otpNum.text.isNotEmpty()) {
+                                startActivity(Intent(applicationContext,
+                                        Verify2Activity::class.java))
+                                otpNum.text.clear()
+                                send.backgroundColor=Color.rgb(77, 182, 172)
+                                resend.backgroundColor=Color.rgb(77, 182, 172)
+
+                            }
+                            else{
+                                toast("Enter OTP First..!!")
+                            }
                         }
                     }
                 }
             }
         }
+
+        send.onClick {
+
+            send.background=buttonBg()
+
+            if (adharNum.text.isNotEmpty()) {
+                if (adharNum.text.length==ADHAR) {
+
+                    resend.visibility=View.VISIBLE
+                    otp.visibility=View.VISIBLE
+                    verify.visibility=View.VISIBLE
+                    toast("OTP Send")
+                    adharNum.text.clear()
+                }
+                else{
+                    toast("Please Enter Valid Adhar Number..!!")
+                }
+            }
+            else{
+                toast("Please Enter Adhar Number First..!!")
+            }
+        }
+    }
+    fun buttonBg() = GradientDrawable().apply {
+        shape = GradientDrawable.RECTANGLE
+        cornerRadius = 6f
+        setStroke(2, Color.rgb(0, 77, 64))
     }
 }
 
--- aadhaar/src/main/java/com/example/aadhaar/Verify2Activity.kt
+++ aadhaar/src/main/java/com/example/aadhaar/Verify2Activity.kt
@@ -1,12 +1,36 @@
 package com.example.aadhaar
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-
+import android.util.Log
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.aadhaar.R.id.imgUser
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_verify2.*
+import org.jetbrains.anko.*
+import org.json.JSONArray
+import org.json.JSONObject
+import org.w3c.dom.Text
+import java.io.InputStream
+
 
 class Verify2Activity : AppCompatActivity() {
+
+    lateinit var imgUser : ImageView
+    lateinit var txtUser : TextView
+    lateinit var txtAdhar : TextView
+    lateinit var txtId : TextView
+    lateinit var txtNumber : TextView
+    lateinit var txtName : TextView
+    lateinit var txtDob : TextView
+    lateinit var txtGender : TextView
+    lateinit var txtAddress : TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +40,115 @@ class Verify2Activity : AppCompatActivity() {
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp)
         toolbar.setNavigationOnClickListener { onBackPressed() }
 
+        alert {
+            customView {
+                verticalLayout {
+                    textView {
+                        text = "Verified Successfully..."
+                        textSize = 30f
+                        typeface = Typeface.DEFAULT_BOLD
+                        backgroundColor = Color.MAGENTA
+                        textColor = Color.WHITE
+                    }
+
+                    linearLayout {
+                        padding = dip(5)
+                        orientation = LinearLayout.HORIZONTAL
+
+                     imgUser=imageView(R.drawable.ic_person_black_24dp)
+                        {
+                            id = R.id.imgUser
+                            backgroundColor = Color.LTGRAY
+                        }.lparams(width = dip(130), height = dip(130)) {
+                            horizontalMargin = dip(5)
+                        }
+
+                        verticalLayout {
+                            padding = dip(10)
+                         txtUser=textView {
+                                id = R.id.txtUser
+                                text = "User_Id : "
+                                textSize = 20f
+                                typeface = Typeface.DEFAULT_BOLD
+                            }
+                         txtId= textView {
+                                id = R.id.txtId
+                                text = "123456789123"
+                                textSize = 20f
+
+                            }.lparams(width = wrapContent, height = wrapContent) {
+                                verticalMargin = dip(8)
+                            }
+                           txtAdhar=textView {
+                                id = R.id.txtAadharnum
+                                text = "Aadhaar_Id : "
+                                textSize = 20f
+                                typeface = Typeface.DEFAULT_BOLD
+                            }
+                           txtNumber=textView {
+                                id = R.id.txtNumber
+                                text = "121212121212"
+                                textSize = 20f
+                            }
+                        }
+                    }
+
+                    padding = dip(5)
+                    txtName=textView {
+                        id = R.id.txtName
+                        text = "Name :"
+                        textSize = 20f
+                        typeface = Typeface.DEFAULT_BOLD
+                    }
+                    txtDob=textView {
+                        id = R.id.txtDob
+                        text = "DOB :"
+                        textSize = 20f
+                        typeface = Typeface.DEFAULT_BOLD
+                    }
+                   txtGender=textView {
+                        id = R.id.txtGender
+                        text = "Gender :"
+                        textSize = 20f
+                        typeface = Typeface.DEFAULT_BOLD
+                    }
+                   txtAddress= textView {
+                        id = R.id.txtAddr
+                        text = "Address :"
+                        textSize = 20f
+                        typeface = Typeface.DEFAULT_BOLD
+                    }
+                }
+            }
+        }.show()
+
+        Picasso.with(applicationContext)
+                .load("http://i.imgur.com")
+                .into(imgUser)
+
+        val ist :InputStream = assets.open("json.txt")
+        val flDt = ist.bufferedReader().use {
+            it.readText()
+        }
+        val rtObj =JSONObject(flDt)
+        val name : String = rtObj.getString("name")
+        val dob : String = rtObj.getString("dob")
+        val gender : String = rtObj.getString("gender")
+        val address : String = rtObj.getString("address")
+
+        Log.i("@codekul","name ${name}")
+        if (txtName.text.isNotEmpty()) {
+            txtName.append(name)
+        }
+        if (txtDob.text.isNotEmpty()) {
+            txtDob.append(dob)
+        }
+        if (txtGender.text.isNotEmpty()) {
+            txtGender.append(gender)
+        }
+        if (txtAddress.text.isNotEmpty()) {
+            txtAddress.append(address)
+        }
 
     }
-
 }
